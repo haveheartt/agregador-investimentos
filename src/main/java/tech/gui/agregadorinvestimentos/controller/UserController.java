926a27712797cd6200d4da9f0baf820c50574680
gui <guilhermev2huehue@gmail.com>
2024-07-19 22:47:07 -0300

    feat: creating controller endpoints

---
+++ src/main/java/tech/gui/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,21 @@
+package tech.gui.agregadorinvestimentos.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.gui.agregadorinvestimentos.entity.User;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody String body){
+        return null;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
+        return null;
+    }
+
+}
