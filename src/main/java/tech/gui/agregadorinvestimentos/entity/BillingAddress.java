dfdc0e76562d801063e5de6c3d81787433bf7a86
gui <guilhermev2huehue@gmail.com>
2024-07-20 14:01:26 -0300

    feat: creating billingAddress entity

---
+++ src/main/java/tech/gui/agregadorinvestimentos/entity/BillingAddress.java
@@ -0,0 +1,56 @@
+package tech.gui.agregadorinvestimentos.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_billing_address")
+public class BillingAddress {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "number")
+    private Integer number;
+
+    public BillingAddress() {
+    }
+
+    public BillingAddress(UUID id, String street, Integer number) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
+
